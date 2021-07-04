cfc98dec8b954b9802814e2516abd6fc54a03592
Bryant Kyle Sagun <bryantkyle.sagun@gmail.com>
2021-07-04 19:22:12 +0800

    Changed PORT

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const router = require('./router')
 const session = require('express-session')
 const flash = require('connect-flash')
 const mongoose = require('mongoose')
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 const dbURI = 'mongodb+srv://tossadmin:tossadmin@cluster0.th2be.mongodb.net/toss?retryWrites=true&w=majority';
 const use = require('@tensorflow-models/universal-sentence-encoder');
 tokenizer = use.load().then(useObj => {
